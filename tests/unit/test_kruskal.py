7ade0d68a7d9c4c27919e8171d4114faaff39879
RegaledSeer <kevinxu1@yahoo.com>
2018-10-27 23:40:57 -0400

    added 3 x 3 test to kruskal's test cases

--- tests/unit/test_kruskal.py
+++ tests/unit/test_kruskal.py
@@ -11,6 +11,62 @@ class TestKruskal(TestCase):
     """Tests to see that kruskal will return the set of edges corresponding
     to the minimum spanning tree from various graphs"""
 
+    def test_three_by_three_graph(self):
+        width = 3
+        height = 3
+        seed = 1
+        g = Graph(width, height, seed)
+        random_min = 0
+        random_max = 10000
+
+        kruskal_edges = kruskal(g)
+
+        self.assertEqual(8, len(kruskal_edges))
+
+        v00 = Vertice("(0, 0)")
+        v01 = Vertice("(0, 1)")
+        v02 = Vertice("(0, 2)")
+        v10 = Vertice("(1, 0)")
+        v11 = Vertice("(1, 1)")
+        v12 = Vertice("(1, 2)")
+        v20 = Vertice("(2, 0)")
+        v21 = Vertice("(2, 1)")
+        v22 = Vertice("(2, 2)")
+
+        # o - o   o
+        # |       |
+        # o - o - o
+        #     |   |
+        # o - o   o
+        random.seed(seed)
+        edge1 = Edge(v00, v01, random.randint(random_min, random_max))
+        Edge(v01, v02, random.randint(random_min, random_max))
+        edge3 = Edge(v10, v11, random.randint(random_min, random_max))
+        edge4 = Edge(v11, v12, random.randint(random_min, random_max))
+        edge5 = Edge(v20, v21, random.randint(random_min, random_max))
+        Edge(v21, v22, random.randint(random_min, random_max))
+
+        edge7 = Edge(v00, v10, random.randint(random_min, random_max))
+        Edge(v01, v11, random.randint(random_min, random_max))
+        edge9 = Edge(v02, v12, random.randint(random_min, random_max))
+        Edge(v10, v20, random.randint(random_min, random_max))
+        edge11 = Edge(v11, v21, random.randint(random_min, random_max))
+        edge12 = Edge(v12, v22, random.randint(random_min, random_max))
+
+        test_edges = sorted([edge1, edge3, edge4, edge5,
+                               edge7, edge9, edge11, edge12],
+                            key=lambda x: x.weight())
+
+        for index in range(len(kruskal_edges)):
+            self.assertEqual(
+                "({0}, {1})".format(
+                    test_edges[index].from_vertice().name(),
+                    test_edges[index].to_vertice().name()),
+                "({0}, {1})".format(
+                    kruskal_edges[index].from_vertice().name(),
+                    kruskal_edges[index].to_vertice().name())
+            )
+
     def test_two_by_three_graph(self):
         width = 2
         height = 3

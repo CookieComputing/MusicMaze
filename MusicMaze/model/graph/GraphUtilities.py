bad9b0a2926dd4f0ff3bcc839f0baa8cab5206a6
RegaledSeer <kevinxu1@yahoo.com>
2018-12-21 00:56:13 -0500

    introduced graph utility class and its test cases

--- MusicMaze/model/graph/GraphUtilities.py
+++ MusicMaze/model/graph/GraphUtilities.py
@@ -40,3 +40,53 @@ def nodes_at_level(graph, root, depth):
             result.append(current_vertice)
             # neighbors at this point are discarded if level >= depth
     return result
+
+
+def shortest_path(graph, from_vertice, to_vertice):
+    """Given two vertices, find a path from the from_vertice leading up to
+    the to_vertice that is the shortest possible. If no such path exists,
+    return an empty list.
+
+    Args:
+        graph(Graph): the graph to look for a path in
+        from_vertice(str): the source vertice
+        to_vertice(str): the destination vertice
+    Returns:
+        list(str): the list containing the path of the vertice
+    Raises:
+        ValueError: If either from or to vertices are not in the graph"""
+    if not (graph.contains_vertice(from_vertice)
+            and graph.contains_vertice(to_vertice)):
+        raise ValueError("Vertice not found in graph")
+
+    parent = {}
+
+    def visit_nodes():
+        work_list = [from_vertice]
+        visited = set()
+
+        while work_list:
+            current_vertice = work_list.pop(0)
+
+            if current_vertice == to_vertice:
+                break
+            visited.add(current_vertice)
+
+            for neighbor in graph.neighbors(current_vertice):
+                if neighbor not in visited:
+                    work_list.append(neighbor)
+                    parent[neighbor] = current_vertice
+
+    def backtrack():
+        result = []
+
+        current_vertice = to_vertice
+        while current_vertice in parent:
+            result.append(current_vertice)
+            current_vertice = parent[current_vertice]
+        if result:
+            result.append(from_vertice)
+        return result
+
+    visit_nodes()
+    return list(reversed(backtrack()))

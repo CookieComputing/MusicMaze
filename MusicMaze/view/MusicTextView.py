f4bac61d64079027d910846139bb4044df9fd096
RegaledSeer <kevinxu1@yahoo.com>
2018-12-25 16:19:41 -0500

    Added starter code for the basis of the text view

---
+++ MusicMaze/view/MusicTextView.py
@@ -0,0 +1,5 @@
+class MusicTextView:
+    """This class represents one instance of a view for the music maze. The
+    purpose of this class is to represent the maze through text and also to
+    create a basis on the methods needed to cover all of the view's expected
+    features for sanity checking purposes."""
---
+++ tests/unit/view/test_musicTextView.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+
+class TestMusicTextView(TestCase):
+    """Test cases in this class are similar to testing the model's string state
+    but with also additional checks for the expected values of the view's
+    interface methods."""
